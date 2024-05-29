b89a2ce9bebcb71324ef3a8cb0beea9f87239ef8
JuShapiro <yuliashepel19@gmail.com>
2024-05-29 13:40:48 +0200

    http-request

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
 import "modern-normalize";
+import axios from "axios";
 import "./App.css";
+import { useEffect, useState } from "react";
+
+const ArticleList = ({ items }) => (
+  <ul>
+    {items.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
 
 const App = () => {
-  return <div></div>;
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    async function fetchArticles() {
+      const response = await axios.get(
+        "https://hn.algolia.com/api/v1/search?query=react"
+      );
+      setArticles(response.data.hits);
+    }
+    fetchArticles();
+  }, [articles]);
+  return (
+    <div>
+      <h1>Latest articles</h1>
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
+  );
 };
 
 export default App;
